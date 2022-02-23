ad5066277bbbb7319c8b6abd4ed8c7170366a7eb
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-02-23 20:48:16 +0100

    Adding toastr notifs. for login & register.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,14 +13,21 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(
       () => this.router.navigate(['/members']),
-      (error) => console.log(error)
+      (error) => {
+        console.log(error)
+        this.toastr.error(error.error);
+      }
     );
   }
 
