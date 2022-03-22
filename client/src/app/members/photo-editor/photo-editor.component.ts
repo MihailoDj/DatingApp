ef232c4f6e3d8f653b39ee7d31ac0cdda3da2dee
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-03-23 00:56:35 +0100

    Implementing the front end for deleting photos.

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -72,4 +72,10 @@ export class PhotoEditorComponent implements OnInit {
       });
     });
   }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(x => x.id !== photoId);
+    });
+  }
 }
