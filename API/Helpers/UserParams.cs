20d5d8bab2140335f3a30c98f4ff435fb4dc8474
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-03-30 23:57:43 +0200

    Fixing issue with the user params for pagination.

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -8,8 +8,8 @@ namespace API.Helpers
     public class UserParams
     {
         public const int MaxPageSize = 50;
-        public int PageNumber { get; set; } = 10;
-        private int _pageSize;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
         public int PageSize
         {
             get { return _pageSize; }
