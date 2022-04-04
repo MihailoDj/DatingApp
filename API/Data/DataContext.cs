d41e7de04f5fc2d50e1fa30a10da356550349490
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-04-04 20:36:47 +0200

    Adding a Like entity & table on the back end.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,20 +10,30 @@ namespace API.Data
     public class DataContext : DbContext
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
 
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     var configuration = new ConfigurationBuilder()
-        //         .SetBasePath(Directory.GetCurrentDirectory())
-        //         .AddJsonFile("appsettings.Development.json")
-        //         .Build();
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            base.OnModelCreating(builder);
 
-        //     var connectionString = configuration.GetConnectionString("datingapp.db");
-        //     optionsBuilder.UseSqlite(connectionString);
-        // }
+            builder.Entity<UserLike>()
+                    .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+
+            builder.Entity<UserLike>()
+                    .HasOne(s => s.SourceUser)
+                    .WithMany(l => l.LikedUsers)
+                    .HasForeignKey(s => s.SourceUserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            
+            builder.Entity<UserLike>()
+                    .HasOne(l => l.LikedUser)
+                    .WithMany(l => l.LikedByUsers)
+                    .HasForeignKey(s => s.LikedUserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
