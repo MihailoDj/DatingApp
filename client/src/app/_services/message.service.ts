ce80d088920e8675635af3705ca541b219b37e5a
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-04-18 22:35:45 +0200

    Adding message sending on the front end.

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -35,6 +35,15 @@ export class MessageService {
   }
 
   getMessageThread(username: string) {
-    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+    return this.http.get<Message[]>(
+      this.baseUrl + 'messages/thread/' + username
+    );
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + 'messages', {
+      recipientUsername: username,
+      content,
+    });
   }
 }
